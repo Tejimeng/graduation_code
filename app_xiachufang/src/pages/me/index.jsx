7e158d652328f2a05dbee265efd8b1a1599bb3c6
Tejimeng <137048130+Tejimeng@users.noreply.github.com>
2024-02-08 18:05:12 +0800

    fix:登录组件布局

--- app_xiachufang/src/pages/me/index.jsx
+++ app_xiachufang/src/pages/me/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from '@/components/Login/index.jsx';
-
+import './index.scss'
 const Index = () => {
     return (
-        <div>
-            <Login login_title_index={1}/>
+        <div className={'me_container'}>
+           <div className='me_login'>
+               <Login login_title_index={1}/>
+           </div>
         </div>
     );
 };
